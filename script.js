36c5595ff4a2acbf49ade8ffb035e82c6b1e7a83
AlbertGR <jonagomez432@gmail.com>
2025-01-30 23:04:29 -0500

    Primer commit: Subiendo mi página web

---
+++ script.js
@@ -0,0 +1,4 @@
+// Ejemplo: Cambiar el color del botón al hacer clic
+document.querySelector('.hero .btn').addEventListener('click', function() {
+    alert("¡Gracias por contactarnos! Te responderemos pronto.");
+});
